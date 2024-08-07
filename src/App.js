4066abd0d526c24942ca5cdf86355f006e9187c6
13w13 <s.weiss@tbs-education.org>
2024-08-07 10:29:22 +0200

    add

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProjectManagement from './components/ProjectManagement';
 import Reports from './components/Reports';
 import DataIntegrationStatus from './components/DataIntegrationStatus';
 
-// Updated import path for mock data
+// Updated import statement
 import { beneficiaries as mockBeneficiaries, activities as mockActivities, projects as mockProjects } from './data/mockData';
 
 const App = () => {
